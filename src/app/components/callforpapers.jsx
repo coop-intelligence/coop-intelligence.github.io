840eeb2547c40c64fab378f4f373fa8c8986f04c
zhongjiaru <jiaruz_bit@163.com>
2025-03-13 11:45:14 +0800

    update callforpapers deadlines

--- src/app/components/callforpapers.jsx
+++ src/app/components/callforpapers.jsx
@@ -28,8 +28,8 @@ const CallForPapers = () => {
             </p>
             <ul className="list-disc list-inside space-y-2 mt-2 mb-8 ml-4">
                 <li>Paper Submission Open: Februray 01, 2025</li>
-                <li><s>Submission asbtract deadline: March 10, 2025</s></li>
-                <li>Paper submission deadline: March 20, 2025</li>
+                <li>Submission asbtract deadline: <s>March 10, 2025</s> March 21, 2025</li>
+                <li>Paper submission deadline:<s>March 20, 2025</s> March 23, 2025</li>
                 <li>Notification of acceptance: March 31, 2025</li>
                 <li>Camera ready: April 07, 2025</li>
             </ul>
