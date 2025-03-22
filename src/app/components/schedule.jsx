02ed2d648d54026ed9f26e4874a98d627d383868
zhongjiaru <jiaruz_bit@163.com>
2025-03-22 16:08:14 +0800

    update schedule

--- src/app/components/schedule.jsx
+++ src/app/components/schedule.jsx
@@ -1,15 +1,11 @@
 const Schedule = () => {
     return <div className="text-justify">
-        <p> The workshop has been accepted as a full-day event. The schedule will be updated in due course.</p>
-        {/* <ul className="list-disc list-inside space-y-2 mt-2 ml-4">
-            <li> <span className="font-bold">13:30</span> - Opening Remark</li>
-            <li><span className="font-bold">13:40</span> - Keynote Talk 1 - Dr. Asim Munawar (IBM Research, US): &quot;LLM-Based Reasoning: Opportunities and Pitfalls &quot;</li>
-            <li><span className="font-bold">14:40</span> - Poster session + Coffee break</li>
-            <li><span className="font-bold">15:50</span> - Keynote Talk 2 - Dr. April Pyone Maung Maung (NII, Japan): &quot;Adversarial Attacks and Defenses on Vision-Language Models&quot;</li>
-            <li><span className="font-bold">16:40</span> - Challenge Award - Dr. Md Anwarus Salam Khan (Softbank, Japan)</li>
-            <li><span className="font-bold">16:55</span> - Closing Remark</li>
-            
-        </ul> */}
+        {/* <p> The workshop has been accepted as a full-day event. 
+            The schedule will be updated in due course.</p> */}
+        <ul className="list-disc list-inside space-y-2 mt-2 ml-4">
+            <li> <span className="font-bold">Date</span> - June 12th (Full Day)</li>
+            <li><span className="font-bold">Agenda</span> - Coming Soon </li>
+        </ul>
     </div>
 }
 export default Schedule;
