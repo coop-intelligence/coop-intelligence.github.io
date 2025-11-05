599d504bd6e5ecefbabc20beaa456ee8cce2f8f6
wyhallenwu <wyhforwork66@gmail.com>
2025-11-05 14:07:53 +0900

    update organizer

--- src/app/components/organizers.jsx
+++ src/app/components/organizers.jsx
@@ -1,12 +1,12 @@
 import PersonAvatar from "./person-avatar";
 
 const Organizers = ()=>{
-    return <div className="text-justify"> 
-         
+    return <div className="text-justify">
         <div className="grid sm:grid-cols-4 grid-cols-2 mt-4 gap-y-6">
             <PersonAvatar name="Xiangbo Gao" image="/xiangbo_gao.png" affiliation={"Texas A&M University"} link={"https://www.xiangbogao.com/"}/>
             <PersonAvatar name="Yuheng Wu" image="/yuheng_wu.png" affiliation={"KAIST"} link={"https://wyhallenwu.github.io/"}/>
             <PersonAvatar name="Dongman Lee" image="/dongman_lee.png" affiliation={"KAIST"} link={"https://cds.kaist.ac.kr/people/dongmanlee"}/>
+            <PersonAvatar name="Haibao Yu" image="/haibao_yu.jpg" affiliation={"University of Hong Kong"} link={"https://www.linkedin.com/in/haibao-yu-152221118"}/>
             <PersonAvatar name="Walter Zimmer" image="/walter_zimmer.jpeg" affiliation={"Technical University of Munich"} link={"https://walzimmer.github.io/website/"}/>
             <PersonAvatar name="Ross Greer" image="/ross.png" affiliation={"Technical University of Munich"} link={"https://www.rossgreer.com/"}/>
             <PersonAvatar name="Bernadette Bucher" image="/Bernadette_Bucher.png" affiliation={"University of Michigan"} link={"https://bucherb.github.io/"}/>
@@ -18,6 +18,7 @@ const Organizers = ()=>{
             <PersonAvatar name="Jiachen Li" image="/jiachen_li.jpg" affiliation={"University of California, Riverside"} link={"https://jiachenli94.github.io/"}/>
             <PersonAvatar name="Ziran Wang" image="/ziran_wang.png" affiliation={"Purdue University"} link={"https://ziranw.github.io/"}/>
             <PersonAvatar name="Yang Zhou" image="/yang_zhou.png" affiliation={"Texas A&M University"} link={"https://sites.google.com/view/yangzhouwebsite"}/>
+            <PersonAvatar name="Hao Yang" image="/hao_yang.png" affiliation={"Johns Hopkins University"} link={"https://www.haofrankyang.net/"}/>
             <PersonAvatar name="Zhengzhong Tu" image="/zhengzhong_tu.png" affiliation={"Texas A&M University"} link={"https://vztu.github.io/"}/>
         </div>
 
